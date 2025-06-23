bf8ba7521ca7a80f324467ca2655ea6d849343a9
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2025-06-22 22:16:30 -0300

    Agrego importaciones en Login, inicializo los estados

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { CartContext } from '../context/CartContext'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+    const { setIsAuth } = useContext(CartContext)
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState({})
+    const navigate = useNavigate()
+
     return (
         <div>
             <h1>Login</h1>
