898e0203e9d5d8fbc95d48c86fcce23ace785779
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2025-06-22 22:08:28 -0300

    Agrego CartContext y CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useContext } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
@@ -10,55 +10,10 @@ import DetalleProducto from './components/DetalleProducto'
 import Login from './pages/Login'
 import Admin from './pages/Admin'
 import RutasProtegidas from './auth/RutasProtegidas'
+import { CartContext } from './context/CartContext'
 
 function App() {
-  const [cart, setCart] = useState([])
-  const [productos, setProductos] = useState([])
-  const [cargando, setCargando] = useState(true)
-  const [error, setError] = useState(false)
-  const [isAuthenticated, setIsAuth] = useState(false)
-
-  useEffect(() => {
-    fetch('https://fakestoreapi.com/products/')
-      .then(respuesta => respuesta.json())
-      .then(datos => {
-        setTimeout(() => {
-          setProductos(datos)
-          setCargando(false)
-        }, 2000)
-      })
-      .catch(error => {
-        console.log('Error ', error)
-        setCargando(false)
-        setError(true)
-      })
-  }, [])
-
-
-  const handleAddToCart = (product) => {
-    const productInCart = cart.find((item) => item.id === product.id)
-    if (productInCart) {
-      setCart(cart.map((item) => item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item))
-    } else {
-      setCart([...cart, { ...product, quantity: 1 }])
-    }
-  }
-
-  const handleDeleteFromCart = (product) => {
-    setCart(prevCart => {
-      return prevCart.map(item => {
-        if (item.id === product.id) {
-          if (item.quantity > 1) {
-            return { ...item, quantity: item.quantity - 1 }
-          } else {
-            return null
-          }
-        } else {
-          return item
-        }
-      }).filter(item => item !== null)
-    })
-  }
+  const { cart, cargando, productos, error, isAuthenticated, handleAddToCart, handleDeleteFromCart } = useContext(CartContext)
 
 
   return (
