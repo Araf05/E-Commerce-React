de8190f65e0f4ec89bbe6f0a40bf446341678f36
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2025-07-02 22:03:08 -0300

    Agrego dependencias, incorporo navbar y carrousel de bootstrap

--- src/components/estaticos/Header.jsx
+++ src/components/estaticos/Header.jsx
@@ -4,48 +4,58 @@ import { Link, NavLink } from 'react-router-dom'
 import './styleEstatico.css'
 import { CartContext } from '../../context/CartContext'
 
+
 const Header = () => {
     const [isCartOpen, setCartOpen] = useState(false)
     const { cart } = useContext(CartContext)
 
     return (
         <header>
-            <div className='hRow' >
-                <ul>
-                    <li className='cartnav'>
-                        <button className='btnCart' onClick={() => setCartOpen(true)}>
-                            <i class="fa-solid fa-cart-shopping"></i>
-                            <span>{cart.length}</span>
-                        </button>
-                        <Cart isOpen={isCartOpen} onClose={() => setCartOpen(false)} />
-                    </li>
-                    <li>
-                        <Link to='/login' className='link'>
-                            <i class="fa-solid fa-user"></i>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <nav>
-                <ul>
-                    <li><NavLink to='/' className={
-                        ({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "link"} >
-                        Inicio
-                    </NavLink></li>
+            <nav className="hRow navbar navbar-expand-lg  bg-body-tertiary">
+                <div>
+                    <ul style={{ width: '100vw' }}>
+                        <li className='cartnav'>
+                            <button className='btnCart' onClick={() => setCartOpen(true)}>
+                                <i className="fa-solid fa-cart-shopping"></i>
+                                <span>{cart.length}</span>
+                            </button>
+                            <Cart isOpen={isCartOpen} onClose={() => setCartOpen(false)} />
+                        </li>
+                        <li>
+                            <Link to='/login' className='link'>
+                                <i className="fa-solid fa-user"></i>
+                            </Link>
+                        </li>
+                        <li>
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
 
-                    <li><NavLink to='/acercade' className={
-                        ({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "link"}>
-                        Sobre nosotros</NavLink></li>
+                <nav className="collapse navbar-collapse colapsable" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item"><NavLink to='/' className={
+                            ({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "link"} >
+                            Inicio
+                        </NavLink></li>
 
-                    <li><NavLink to='/productos' className={
-                        ({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "link"}>
-                        Galeria de productos</NavLink></li>
+                        <li className="nav-item"><NavLink to='/acercade' className={
+                            ({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "link"}>
+                            Sobre nosotros</NavLink></li>
 
-                    <li><NavLink to='/contacto' className={
-                        ({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "link"}>
-                        Contacto</NavLink></li>
-                </ul>
+                        <li className="nav-item"><NavLink to='/productos' className={
+                            ({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "link"}>
+                            Galeria de productos</NavLink></li>
+
+                        <li className="nav-item"><NavLink to='/contacto' className={
+                            ({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "link"}>
+                            Contacto</NavLink></li>
+                    </ul>
+                </nav>
             </nav>
+
         </header >
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,26 @@ import Header from '../components/estaticos/Header'
 import Footer from '../components/estaticos/Footer'
 import ProductList from '../components/ProductList'
 import loading from '../assets/load-1110_256.gif'
-import banner from '../assets/banner.jpg'
+import ofertas from '../assets/banner.jpg'
+import promociones from '../assets/banner2.jpg'
+import Carousel from '../components/estaticos/Carousel'
 import { CartContext } from '../context/CartContext'
 
 const Home = () => {
-    const { cargando} = useContext(CartContext)
+    const { cargando } = useContext(CartContext)
 
 
     return (
         <>
             <Header />
             <main>
-                <div style={{ width: '100%' }}>
+                <Carousel ofertas={ofertas} promociones={promociones} />
+                {/* <div style={{ width: '100%' }}>
                     <img style={{ width: '100%', height: 'auto', maxHeight: '400px', objectFit: 'content', display: 'block', margin: '0 auto' }} src={banner} alt="banner" />
-                </div>
+                </div> */}
                 {
                     cargando ? <img src={loading} alt='loading' /> :
-                        <ProductList/>
+                        <ProductList />
                 }
             </main>
             <Footer />
