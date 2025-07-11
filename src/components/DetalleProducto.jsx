0ec1bdd871fd6aaeef76e0c9a19854d06758f129
Araceli Noemí Fernández <aracelifernandez.3006@gmail.com>
2025-07-10 23:22:32 -0300

    Agrego detalles al producto, estructura

--- src/components/DetalleProducto.jsx
+++ src/components/DetalleProducto.jsx
@@ -19,20 +19,41 @@ const DetalleProducto = ({ productos, cargando }) => {
                         <>
                             <div style={{
                                 display: 'flex',
-                                flexDirection: 'column',
-                                padding: '15px'
+                                flexDirection: 'row',
+                                padding: '15px',
+                                gap: '15px'
                             }}>
-                                <div>
+                                <div style={{
+                                    width: '300px',
+                                    height: '400px',
+                                    border: '1px solid black'
+                                }}>
                                     <img src={product.image} alt={product.name}
                                         style={{
-                                            width: '200px'
+                                            width: '100%',
+                                            height: '100%',
+                                            objectFit: 'cover'
                                         }} />
                                 </div>
-                                <h2>{product.name}</h2>
-                                <p>stock: {product.stock}</p>
-                                <h3>${product.price}</h3>
-                                <button style={{ width: 'fit-content' }}>Agregar</button>
-                                <p>{product.description}</p>
+                                <div style={{ width: '100%' }}>
+                                    <h2>{product.name}</h2>
+                                    <hr />
+                                    <p>Stock: {product.stock}</p>
+                                    <p>Marca: {product.brand}</p>
+                                    <h3>${product.price}</h3>
+                                    <p>Disponible para:</p>
+                                    <label>
+                                        <input type="radio" name='checkEnvio' value='domicilio' />
+                                        🛻Envío a domicilio
+                                    </label>
+                                    <label>
+                                        <input type="radio" name='checkEnvio' value='local' />
+                                        🏪Retiro en el local
+                                    </label>
+                                    <button style={{ width: 'fit-content' }}>Agregar</button>
+                                    <p>Descripción del producto:</p>
+                                    <p>{product.description}</p>
+                                </div>
                             </div>
                         </>
 
